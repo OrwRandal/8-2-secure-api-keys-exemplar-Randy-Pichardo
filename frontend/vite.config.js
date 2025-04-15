4ec6405f61f011a3d94e8b2b8c136518b9be442f
orwrandal <randypichardo1987@gmail.com>
2025-04-15 14:43:56 -0400

    Updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 const SERVER_PORT = 8080;
 
-// https://vitejs.dev/config/
+// I set up this proxy so that when I'm running both the frontend and backend locally,
+// any request from the frontend to an endpoint starting with /api will get forwarded to the backend on port 8080.
+// Without this, the frontend would try to fetch from its own port (in this case, 5173), which wouldn’t reach the backend.
+
 export default defineConfig({
   plugins: [react()],
   server: {
